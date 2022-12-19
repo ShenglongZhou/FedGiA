2ca65db43763eab6fcf05940e85e1274a39f12c0
Shenglong Zhou <36698633+ShenglongZhou@users.noreply.github.com>
2022-12-19 22:56:15 +0000

    Update PlotObj.m

--- FedGiA/Solver/PlotObj.m
+++ FedGiA/Solver/PlotObj.m
@@ -1,12 +1,12 @@
-function pt = PlotObj(obj1)
+function  PlotObj(obj1)
     figure('Renderer', 'painters', 'Position',[1100 400 370 320]);
-    axes('Position', [0.14 0.14 0.83 0.8] ); 
-    h1 = plot(1:length(obj1),obj1(1:end)); hold on 
-    grid on
-    h1.LineWidth  = 1.5;   %h2.LineWidth  = 1.5;       
-    h1.LineStyle  = '-';   %h2.LineStyle  = '--'; 
-    h1.Color = '#3caea3';  %h2.Color = '#ed553b'; 
+    axes('Position', [0.16 0.14 0.81 0.8] ); 
+    h1 = plot(1:length(obj1),obj1(1:end)); 
+    hold on, grid on
+    h1.LineWidth  = 1.5;        
+    h1.LineStyle  = '-';  
+    h1.Color      = '#3caea3';  
     xlabel('Iterations'); ylabel('Objective');  
-    pt   = 1;
+    legend('FedGiA')
 end
 
