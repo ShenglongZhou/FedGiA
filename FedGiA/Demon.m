19ab723ac81e2aaab9172a91d684512f17cb4d7a
Shenglong Zhou <36698633+ShenglongZhou@users.noreply.github.com>
2022-12-19 22:32:35 +0000

    Update Demon.m

--- FedGiA/Demon.m
+++ FedGiA/Demon.m
@@ -2,38 +2,31 @@ clc; clear; close all;
 addpath(genpath(pwd));
 
 t     = 1;
-prob  = {'LinReg','LogReg','NCLogReg'};
-m     = 128;
-n     = 100;   
-switch prob{t}
-    case 'LinReg'
-        dim   = randi([50 150],1,m);
-        d     = sum(dim);    
-        [A,b] = GenerateData(m,n,dim,1/3,1/3); 
-    otherwise
-        dat   = load('toxicity.mat'); 
-        lab   = load('toxicityclass.mat'); 
-        lab.y(lab.y==-1)= 0;
-        A     = Normalization(dat.X,3); 
-        b     = lab.y;
-        [d,n] = size(A);  
-        I     = randperm(d);
-        A     = A(I,:);  % randomize samples
-        b     = b(I,:);  
-        while 1
-            idx = unique([randperm(d-2,m-1)+1 d]);
-            dim  = idx-[0 idx(1:end-1)]; 
-            if min(dim)>0.01*d/m; break; end
-        end 
-end
+Prob  = {'LinReg','LogReg','NCLogReg'};
+m     = 100;
+prob  = Prob{t};
 
-k0        = 10;
-pars.r0   = 1; % increase this value if the solver diverges 
+switch prob 
+    case 'LinReg' 
+        n       = 100;
+        var1    = 1/3;
+        var2    = 1/3; 
+    case {'LogReg','NCLogReg'}
+        var1    = load('toxicity.mat').X; 
+        var2    = load('toxicityclass.mat').y; 
+        n       = size(var1,2);  
 
-pars.optH = 'diag';
-out1      = FedGiA(dim,n,A,b,k0,prob{t},pars);   
-pt1       = PlotObj(out1.objx);
+end 
  
-pars.optH = 'gram';
-out2      = FedGiA(dim,n,A,b,k0,prob{t},pars);   
-pt2       = PlotObj(out2.objx);
+[A,b,dim,n] = DataGeneration(prob,m,n,var1,var2); 
+ 
+k0          = 10;
+pars.r0     = 1; % increase this value if the solver diverges 
+
+pars.optH   = 'diag';
+out1        = FedGiA(dim,n,A,b,k0,prob,pars);   
+pt1         = PlotObj(out1.objx);
+ 
+pars.optH   = 'gram';
+out2        = FedGiA(dim,n,A,b,k0,prob,pars);   
+pt2         = PlotObj(out2.objx);
