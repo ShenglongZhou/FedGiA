fed8e58defac5cea7a6b697d8f7964a912999a73
Shenglong Zhou <36698633+ShenglongZhou@users.noreply.github.com>
2022-12-19 22:32:57 +0000

    Update Demon_obj_iter.m

--- FedGiA/Demon_obj_iter.m
+++ FedGiA/Demon_obj_iter.m
@@ -1,34 +1,47 @@
 clc; clear; close all;
 addpath(genpath(pwd));
  
+ 
+t     = 3;
+Prob  = {'LinReg','LogReg','NCLogReg'};
 m     = 128;
-n     = 100; 
-
+prob  = Prob{t};
 k0    = [20 15 10 5 1];
-di    = randi([50 100],1,m);
-d     = sum(di);    
-[A,b] = GenerateData(m,n,di,1/3,1/3);
+
+switch prob 
+    case 'LinReg' 
+        n       = 100;
+        var1    = 1/3;
+        var2    = 1/3; 
+    case {'LogReg','NCLogReg'}
+        var1    = load('toxicity.mat').X; 
+        var2    = load('toxicityclass.mat').y; 
+        n       = size(var1,2);  
+
+end  
+
+[A,b,dim,n] = DataGeneration(prob,m,n,var1,var2); 
  
 pars.optH   = 'gram'; 
 for i       = 1:nnz(k0)   
-    out0{i} = FedGiA(di,n,A,b,k0(i),'LinReg',pars);
+    out0{i} = FedGiA(dim,n,A,b,k0(i),'LinReg',pars);
 end
  
 pars.optH   = 'diag';
 for i       = 1:nnz(k0)
-    out1{i} = FedGiA(di,n,A,b,k0(i),'LinReg',pars);
+    out1{i} = FedGiA(dim,n,A,b,k0(i),'LinReg',pars);
 end
 
 
 % present the results
 figure('Renderer', 'painters', 'Position',[800 400 700 330]);
-axes('Position', [0.06 0.13 0.91 0.82] ); 
+axes('Position', [0.09 0.13 0.91 0.82] ); 
 colors = {'#173f5f','#20639b','#3caea3','#f6d55c','#ed553b'}; 
 for t = 1 : 2
     sub = subplot(1,2,t);               
     for i = 1:nnz(k0) 
         if t==1
-           y = out0{i}.objx(1:end-1); plt=@plot;
+           y = out0{i}.objx; plt=@plot;
         else
            y = out0{i}.errx; plt=@semilogy;
         end  
